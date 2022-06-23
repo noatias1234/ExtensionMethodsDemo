14d22b4596e92056b7fe2d3bd0dac68a029035f8
noatias1234 <noatias1234@gmail.com>
2022-06-23 11:07:46 +0300

    Add project files.

---
+++ ExtensionMethodsDemo/IntExtension.cs
@@ -0,0 +1,15 @@
+﻿namespace ExtensionMethodsDemo;
+
+public static class IntExtension
+{
+    public static int Pow(this int num, int exp)
+    {
+        return (int)Math.Pow(num, exp);        
+    }
+
+
+    public static string ConcatTwoNumsToStr(this int num,int num2,string str)
+    {
+        return $"{num} - {str} - {num2}";
+    }
+}
---
+++ ExtensionMethodsDemo/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ExtensionMethodsDemo;
+
+Console.WriteLine("Hello, World!");
+
+int n1 = 2;
+Console.WriteLine(n1.Pow(3));
+Console.WriteLine(n1.ConcatTwoNumsToStr(147,"Bar"));
+
